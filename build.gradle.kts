98078b819bc7f50ad79572f5982e4c2267c6b90c
Mario Schwede <102790699+mario-schwede-hivemq@users.noreply.github.com>
2022-06-13 12:44:46 +0200

    PUBSUB: Customization SDK API Suggestion (#1)
    - Add API suggestion for the PubSub customization SDK.

---
+++ build.gradle.kts
@@ -0,0 +1,164 @@
+buildscript {
+    if (gradle.includedBuilds.any { it.name == "plugins" }) {
+        plugins {
+            id("com.hivemq.version-updater")
+        }
+    }
+}
+
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin")
+    id("io.github.sgtsilvio.gradle.defaults")
+    id("com.github.sgtsilvio.gradle.metadata")
+    id("com.github.sgtsilvio.gradle.javadoc-links")
+    id("com.github.hierynomus.license")
+}
+
+plugins.withId("com.hivemq.version-updater") {
+    project.ext.set("versionUpdaterFiles", arrayOf("README.adoc"))
+}
+
+group = "com.hivemq"
+description = "SDK for the development of HiveMQ PubSub Extension customizations"
+
+metadata {
+    readableName.set("HiveMQ PubSub Extension Customization SDK")
+    organization {
+        name.set("HiveMQ GmbH")
+        url.set("https://www.hivemq.com/")
+    }
+    license {
+        apache2()
+    }
+    developers {
+        register("hurtadosanti") {
+            fullName.set("Santiago Hurtado")
+            email.set("santiago.hurtado@hivemq.com")
+        }
+        register("Donnerbart") {
+            fullName.set("David Sondermann")
+            email.set("david.sondermann@hivemq.com")
+        }
+        register("YannickWeber") {
+            fullName.set("Yannick Weber")
+            email.set("yannick.weber@hivemq.com")
+        }
+        register("Florian-Limpoeck") {
+            fullName.set("Florian Limpöck")
+            email.set("florian.limpoeck@hivemq.com")
+        }
+        register("mario-schwede-hivemq") {
+            fullName.set("Mario Schwede")
+            email.set("mario.schwede@hivemq.com")
+        }
+    }
+    github {
+        org.set("hivemq")
+        repo.set("hivemq-pubsub-extension-customization-sdk")
+        issues()
+    }
+}
+
+/* ******************** dependencies ******************** */
+
+val internalPlatform by configurations.creating {
+    isVisible = false
+    isCanBeConsumed = false
+    isCanBeResolved = false
+}
+
+configurations {
+    compileClasspath.get().extendsFrom(internalPlatform)
+    runtimeClasspath.get().extendsFrom(internalPlatform)
+    testCompileClasspath.get().extendsFrom(internalPlatform)
+    testRuntimeClasspath.get().extendsFrom(internalPlatform)
+    javadocLinks.get().shouldResolveConsistentlyWith(runtimeClasspath.get())
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    internalPlatform(platform("com.hivemq:hivemq-main-platform"))
+
+    api("com.hivemq:hivemq-extension-sdk:$version")
+    api("org.slf4j:slf4j-api")
+}
+
+/* ******************** java ******************** */
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<Jar>().configureEach {
+    manifest.attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Vendor" to metadata.organization.get().name.get(),
+            "Implementation-Version" to project.version
+    )
+}
+
+tasks.javadoc {
+    title = "${metadata.readableName.get()} ${project.version} API"
+
+    doLast { // javadoc search fix for jdk 11 https://bugs.openjdk.java.net/browse/JDK-8215291
+        copy {
+            from(destinationDir!!.resolve("search.js"))
+            into(temporaryDir)
+            filter { line -> line.replace("if (ui.item.p == item.l) {", "if (item.m && ui.item.p == item.l) {") }
+        }
+        delete(destinationDir!!.resolve("search.js"))
+        copy {
+            from(temporaryDir.resolve("search.js"))
+            into(destinationDir!!)
+        }
+    }
+}
+
+/* ******************** publishing ******************** */
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            from(components["java"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionResult()
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signKey: String? by project
+    val signKeyPass: String? by project
+    useInMemoryPgpKeys(signKey, signKeyPass)
+    sign(publishing.publications["maven"])
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+/* ******************** checks ******************** */
+
+license {
+    header = file("HEADER")
+    mapping("java", "SLASHSTAR_STYLE")
+}
---
+++ src/main/java/com/hivemq/extensions/pubsub/api/model/Timestamp.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hivemq.extensions.pubsub.api.model;
+
+import com.hivemq.extension.sdk.api.annotations.DoNotImplement;
+import com.hivemq.extension.sdk.api.annotations.Immutable;
+
+/**
+ * Timestamp that represents a point in time from epoch (UTC 00:00:00 01.01.1970), encoded as a count of seconds and
+ * fractions of seconds at nanosecond resolution.
+ *
+ * @author Florian Limpöck
+ * @author Mario Schwede
+ * @since 4.9.0
+ */
+@Immutable
+@DoNotImplement
+public interface Timestamp {
+
+    /**
+     * @return count of seconds from epoch.
+     * @since 4.9.0
+     */
+    long getSeconds();
+
+    /**
+     * @return fractions of seconds at nanosecond resolution.
+     * @since 4.9.0
+     */
+    int getNanos();
+
+    /**
+     * @return count of milliseconds from epoch. The applied conversion truncates, so lose precision.
+     * @since 4.9.0
+     */
+    long toMillis();
+}
