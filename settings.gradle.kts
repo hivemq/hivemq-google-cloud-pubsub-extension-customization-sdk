20a6934bcd655147ca6ca027d050971c500457a1
Silvio Giebl <silvio.giebl@hivemq.com>
2023-09-28 20:55:15 +0200

    Use version catalog for plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,4 @@
 pluginManagement {
-    plugins {
-        id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus-publish.version"]}"
-        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
-        id("io.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
-        id("io.github.sgtsilvio.gradle.javadoc-links") version "${extra["plugin.javadoc-links.version"]}"
-        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-    }
-
     if (file("../hivemq/plugins").exists()) {
         includeBuild("../hivemq/plugins")
     }
