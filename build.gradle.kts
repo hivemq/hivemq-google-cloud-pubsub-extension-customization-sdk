hivemq/hivemq-kafka-extension-customization-sdk

build > use = instead of .set()

build.gradle.kts
@@ -18,41 +18,41 @@ plugins {
 }
 
 plugins.withId("com.hivemq.version-updater") {
-    project.ext.set("versionUpdaterFiles", arrayOf("README.adoc"))
+    project.ext["versionUpdaterFiles"] = arrayOf("README.adoc")
 }
 
 group = "com.hivemq"
 description = "SDK for the development of HiveMQ Kafka Extension customizations"
 
 metadata {
-    readableName.set("HiveMQ Kafka Extension Customization SDK")
+    readableName = "HiveMQ Kafka Extension Customization SDK"
     organization {
-        name.set("HiveMQ GmbH")
-        url.set("https://www.hivemq.com/")
+        name = "HiveMQ GmbH"
+        url = "https://www.hivemq.com/"
     }
     license {
         apache2()
     }
     developers {
         register("cschaebe") {
-            fullName.set("Christoph Schaebel")
-            email.set("christoph.schaebel@hivemq.com")
+            fullName = "Christoph Schaebel"
+            email = "christoph.schaebel@hivemq.com"
         }
         register("lbrandl") {
-            fullName.set("Lukas Brandl")
-            email.set("lukas.brandl@hivemq.com")
+            fullName = "Lukas Brandl"
+            email = "lukas.brandl@hivemq.com"
         }
         register("flimpoeck") {
-            fullName.set("Florian Limpoeck")
-            email.set("florian.limpoeck@hivemq.com")
+            fullName = "Florian Limpoeck"
+            email = "florian.limpoeck@hivemq.com"
         }
         register("sauroter") {
-            fullName.set("Georg Held")
-            email.set("georg.held@hivemq.com")
+            fullName = "Georg Held"
+            email = "georg.held@hivemq.com"
         }
         register("SgtSilvio") {
-            fullName.set("Silvio Giebl")
-            email.set("silvio.giebl@hivemq.com")
+            fullName = "Silvio Giebl"
+            email = "silvio.giebl@hivemq.com"
         }
     }
     github {
@@ -73,7 +73,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion = JavaLanguageVersion.of(11)
     }
     withJavadocJar()
     withSourcesJar()

hivemq/hivemq-google-cloud-pubsub-extension-customization-sdk

build.gradle.kts
@@ -18,41 +18,41 @@ plugins {
 }
 
 plugins.withId("com.hivemq.version-updater") {
-    project.ext.set("versionUpdaterFiles", arrayOf("README.adoc"))
+    project.ext["versionUpdaterFiles"] = arrayOf("README.adoc")
 }
 
 group = "com.hivemq"
 description = "SDK for the development of HiveMQ Google Cloud Pub/Sub Extension customizations"
 
 metadata {
-    readableName.set("HiveMQ Google Cloud Pub/Sub Extension Customization SDK")
+    readableName = "HiveMQ Google Cloud Pub/Sub Extension Customization SDK"
     organization {
-        name.set("HiveMQ GmbH")
-        url.set("https://www.hivemq.com/")
+        name = "HiveMQ GmbH"
+        url = "https://www.hivemq.com/"
     }
     license {
         apache2()
     }
     developers {
         register("hurtadosanti") {
-            fullName.set("Santiago Hurtado")
-            email.set("santiago.hurtado@hivemq.com")
+            fullName = "Santiago Hurtado"
+            email = "santiago.hurtado@hivemq.com"
         }
         register("Donnerbart") {
-            fullName.set("David Sondermann")
-            email.set("david.sondermann@hivemq.com")
+            fullName = "David Sondermann"
+            email = "david.sondermann@hivemq.com"
         }
         register("YannickWeber") {
-            fullName.set("Yannick Weber")
-            email.set("yannick.weber@hivemq.com")
+            fullName = "Yannick Weber"
+            email = "yannick.weber@hivemq.com"
         }
         register("Florian-Limpoeck") {
-            fullName.set("Florian Limpöck")
-            email.set("florian.limpoeck@hivemq.com")
+            fullName = "Florian Limpöck"
+            email = "florian.limpoeck@hivemq.com"
         }
         register("mario-schwede-hivemq") {
-            fullName.set("Mario Schwede")
-            email.set("mario.schwede@hivemq.com")
+            fullName = "Mario Schwede"
+            email = "mario.schwede@hivemq.com"
         }
     }
     github {
@@ -73,7 +73,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion = JavaLanguageVersion.of(11)
     }
     withJavadocJar()
     withSourcesJar()